7a5466b6577759256b8f87345fc6582c9ce6e02c
spinlock <dixon@atomicbroadcast.net>
2010-10-25 14:49:11 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '0a5d11e5f4e6be5ecf809e6d79bf29802039195c0c18c683d6625e4caa63f86fe798dcb05883d24a329b5d06efb6ee48446c1ce85d35313129d0c249a89ae185'

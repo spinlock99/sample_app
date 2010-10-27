624b5e5de7ddabcb87dd76c332ea8c168486daea
spinlock <dixon@atomicbroadcast.net>
2010-10-27 09:19:45 -0700

    Implemented sign-in and sign-out.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -84,4 +84,9 @@ RSpec.configure do |config|
   # examples within a transaction, remove the following line or assign false
   # instead of true.
   config.use_transactional_fixtures = true
+
+  # Simulate user signing in inside tests.
+  def test_sign_in(user)
+    controller.sign_in(user)
+  end
 end
